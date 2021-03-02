82706718b0057f3a7162dd14bbbda758962b2a76
Andrea <andrea3aprile99@gmail.com>
2021-03-02 16:21:14 +0100

    color

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 for ( var i = 1 ; i <= 100 ; i++ ){
   if ((i % 3 == 0) && (i % 5 == 0)){
-    document.write("FizzBuzz" + "<br>");
+    document.write("<div class=\"red\">3 e 5</div>" + "<br>");
   } else if (i % 3 == 0){
-    document.write("Fizz" + "<br>");
+    document.write("<div class=\"red1\">3</div>" + "<br>");
   } else if (i % 5 == 0){
-    document.write("Buzz" + "<br>");
+    document.write("<div class=\"red2\">5</div>" + "<br>");
   }  else {
     document.write(i+ "<br>");
   }
