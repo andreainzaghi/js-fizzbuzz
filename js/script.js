52be856273f99ecc74ca03c73426fd0f3dc7acdc
Andrea <andrea3aprile99@gmail.com>
2021-03-02 15:13:55 +0100

    f

--- js/script.js
+++ js/script.js
@@ -9,19 +9,3 @@ for ( var i = 1 ; i <= 100 ; i++ ){
     document.write(i+ "<br>");
   }
 }
-
-//
-// for (var i = 1; i <= 100; i++) {
-//   if ( i % 3 == 0 ) {
-//     console.log("Fizz" + "<br>");
-//   }
-//   else if ( i % 5 == 0 ) {
-//     console.log("Buzz" + "<br>");
-//   }
-//   else if ( i % 3 == 0 && i % 5 == 0 ) {
-//     console.log("FizzBuzz" + "<br>");
-//   }
-//   else {
-//     console.log(i);
-//   }
-// }
