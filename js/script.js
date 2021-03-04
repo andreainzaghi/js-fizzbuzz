cb3b99124ba6a219bb564ee1c60e785b6473a5fd
Andrea <andrea3aprile99@gmail.com>
2021-03-04 11:30:05 +0100

    d

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 for ( var i = 1 ; i <= 100 ; i++ ){
   if ((i % 3 == 0) && (i % 5 == 0)){
-    document.write("<div class=\"red\">3 e 5</div>" + "<br>");
+    document.write("<div class=\"red\">fizzbuzz</div>" + "<br>");
   } else if (i % 3 == 0){
-    document.write("<div class=\"red1\">3</div>" + "<br>");
+    document.write("<div class=\"red1\">fizz</div>" + "<br>");
   } else if (i % 5 == 0){
-    document.write("<div class=\"red2\">5</div>" + "<br>");
+    document.write("<div class=\"red2\">buzz</div>" + "<br>");
   }  else {
     document.write(i+ "<br>");
   }
